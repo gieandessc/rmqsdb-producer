7767c13800c7f5510daf528d5b803fa3e6c46eca
Gieandes Silva <courses.gieandessilva@gmail.com>
2021-03-11 17:22:23 -0300

    [rmqsdb] Início do Projeto

---
+++ src/main/java/com/example/rmqsbd/producer/RmqsbdProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rmqsbd.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmqsbdProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmqsbdProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rmqsbd/producer/RmqsbdProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rmqsbd.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmqsbdProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
